1658bb31942a8165dba549a59aaad233eb9b01f2
sasha peskovskii <saskapeskovsky@MacBook-Pro--Sasha.local>
2026-05-14 18:32:31 +0300

    update

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
 import os
 import telebot
-
 from parser import get_tomorrow, get_week
 
 TOKEN = os.getenv("TOKEN")
+
+print("TOKEN =", TOKEN)  # 👈 диагностика
+
+if not TOKEN:
+    raise Exception("TOKEN is not set in environment variables")
 
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(
-        message.chat.id,
-        "/tomorrow — завтра\n/week — неделя"
-    )
+    bot.send_message(message.chat.id, "/tomorrow — завтра\n/week — неделя")
 
 
 @bot.message_handler(commands=["tomorrow"])
